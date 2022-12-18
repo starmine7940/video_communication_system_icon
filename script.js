129e19872513356b80ffbdc30efc16cbc261eb48
starmine7940 <starmine7940@gmail.com>
2022-12-18 20:53:53 +0900

    audioをtrueに変更

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const Peer = window.Peer;
 
     const localStream = await navigator.mediaDevices.getUserMedia({
         video: true,
-        audio: false,
+        audio: true,
     });
     myVideo.srcObject = localStream;
     
